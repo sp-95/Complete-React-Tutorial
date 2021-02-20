7e3a6fa1e62a25ae25a915f3f7f305d95433bae6
Shashanka Prajapati <40879986+sp-fm@users.noreply.github.com>
2021-02-20 13:57:47 +0545

    Test click events in the homepage

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,7 +1,13 @@
 const Home = () => {
+    const handleClick = e => console.log("Hello, world!", e);
+
+    const handleClickAgain = (name, e) => console.log(`Hello ${name}`, e.target);
+
     return (
         <div className="home">
             <h2>Homepage</h2>
+            <button onClick={handleClick}>Click Me</button>
+            <button onClick={(e) => handleClickAgain("Shashanka", e)}>Click me again</button>
         </div>
     );
 }
