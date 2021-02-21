434ed6751b2eb913531cf4e620be8294b3bea468
Shashanka Prajapati <shashankap95@gmail.com>
2021-02-21 22:17:59 +0545

    Read blogs from the mock data

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -2,23 +2,17 @@ import { useEffect, useState } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
-    const [blogs, setBlogs] = useState([
-        { title: 'My new website', body: 'lorem ipsum...', author: 'mario', id: 1 },
-        { title: 'Welcome party!', body: 'lorem ipsum...', author: 'yoshi', id: 2 },
-        { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'mario', id: 3 }
-    ])
-
-    const handleDelete = (id) => {
-        setBlogs(prev => prev.filter(blog => blog.id !== id));
-    }
+    const [blogs, setBlogs] = useState(null);
 
     useEffect(() => {
-        console.log('use effect ran');
+        fetch("http://localhost:8000/blogs")
+            .then(response => response.json())
+            .then(data => setBlogs(data));
     }, []);
 
     return (
         <div className="home">
-            <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete} />
+            {blogs && <BlogList blogs={blogs} title="All Blogs!" />}
         </div>
     );
 }
