8ccfdc45cfe75082dbea5bd3ff1fd560a24ddf71
Shashanka Prajapati <shashankap95@gmail.com>
2021-02-21 22:45:59 +0545

    Fetch the data using a reusable custom react hook

---
+++ dojo-blog/src/useFetch.js
@@ -0,0 +1,30 @@
+import { useEffect, useState } from 'react';
+
+const useFetch = (url) => {
+    const [data, setData] = useState(null);
+    const [isPending, setIsPending] = useState(true);
+    const [error, setError] = useState(null);
+
+    useEffect(() => {
+        setTimeout(() => {
+            fetch(url)
+                .then(response => {
+                    if (!response.ok) throw Error("Couldn't fetch the data for that resource");
+                    return response.json();
+                })
+                .then(data => {
+                    setData(data);
+                    setIsPending(false);
+                    setError(null);
+                })
+                .catch(err => {
+                    setError(err.message);
+                    setIsPending(false);
+                });
+        }, 1000);
+    }, [url]);
+
+    return { data, isPending, error };
+}
+
+export default useFetch;
