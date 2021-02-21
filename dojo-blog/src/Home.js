dd142e5bc150f34a0e446e2815e7520d03c20ebf
Shashanka Prajapati <40879986+sp-fm@users.noreply.github.com>
2021-02-21 10:18:36 +0545

    Set and change variables using useState hook

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,15 +1,22 @@
-const Home = () => {
-    const handleClick = e => console.log("Hello, world!", e);
+import {useState} from 'react';
 
-    const handleClickAgain = (name, e) => console.log(`Hello ${name}`, e.target);
+const Home = () => {
+    // let name = 'mario';
+    const [name, setName] = useState('mario');
+    const [age, setAge] = useState(25);
+
+    const handleClick = () => {
+        setName('luigi')
+        setAge(30);
+    };
 
     return (
         <div className="home">
             <h2>Homepage</h2>
+            <p>{name} is {age} years old</p>
             <button onClick={handleClick}>Click Me</button>
-            <button onClick={(e) => handleClickAgain("Shashanka", e)}>Click me again</button>
         </div>
     );
 }
- 
+
 export default Home;
