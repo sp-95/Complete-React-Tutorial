385dad4015a683687a49ddbe934c2c948b2c37b3
Shashanka Prajapati <shashankap95@gmail.com>
2021-02-22 11:31:01 +0545

    Create a form to create a new blog

--- dojo-blog/src/Create.js
+++ dojo-blog/src/Create.js
@@ -1,9 +1,43 @@
+import { useState } from "react";
+
 const Create = () => {
+    const [title, setTitle] = useState('');
+    const [body, setBody] = useState('');
+    const [author, setAuthor] = useState('Mario');
+
     return (
         <div className="create">
             <h2>Add a new Blog</h2>
+            <form>
+                <label>Blog Title:</label>
+                <input
+                    type="text"
+                    value={title}
+                    onChange={({ target }) => setTitle(target.value)}
+                    required
+                />
+                <label>Blog Body:</label>
+                <textarea
+                    value={body}
+                    onChange={({ target }) => setBody(target.value)}
+                    required
+                ></textarea>
+                <label>Blog Author:</label>
+                <select
+                    value={author}
+                    onChange={({target}) => setAuthor(target.value)}
+                >
+                    <option value="Mario">Mario</option>
+                    <option value="Luigi">Luigi</option>
+                    <option value="Yoshi">Yoshi</option>
+                </select>
+                <button>Add blog</button>
+                <p>{title}</p>
+                <p>{body}</p>
+                <p>{author}</p>
+            </form>
         </div>
     );
 }
- 
+
 export default Create;
